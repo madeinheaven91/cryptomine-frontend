0f85bf11e9f198e7df267cc7d90efb0d7124864a
madeinheaven91 <exspectanss@gmail.com>
2024-06-18 18:27:56 +0300

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,29 @@
+<script setup>
+import Pickaxe from './components/icons/Pickaxe.vue'
+</script>
+
+<template>
+  <header class="flex">
+    <!-- avatar -->
+    <!-- nickname -->
+  </header>
+  <main class="flex flex-col gap-10">
+    <div class="flex gap-5">
+      <div class="flex flex-col gap-10 rounded-xl border">
+        <p>За тап</p>
+        <p>1000</p>
+      </div>
+      <div class="flex flex-col gap-10 rounded-xl border">
+        <p>За тап</p>
+        <p>1000</p>
+      </div>
+      <div class="flex flex-col gap-10 rounded-xl border">
+        <p>За тап</p>
+        <p>1000</p>
+      </div>
+    </div>
+    <Pickaxe color="#111111"/>
+  </main>
+</template>
+
+<style scoped></style>
