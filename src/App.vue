053c26cb193f14ecc48301f822dd9b87d3eb2e7a
madeinheaven91 <exspectanss@gmail.com>
2024-06-25 21:39:45 +0300

    i need to make api work

--- src/App.vue
+++ src/App.vue
@@ -4,6 +4,13 @@ import Pickaxe from './components/icons/Pickaxe.vue'
 import Header from './components/Header.vue'
 import Block from './components/Block.vue'
 import BottomPanel from './components/BottomPanel.vue'
+import axios from 'axios'
+
+const serverURL = 'http://localhost:3000'
+
+const user = axios.get(`${serverURL}/api/user/1`)
+console.log(user)
+console.log("bebra")
 
 let moneyTotal = ref(0)
 let perClick = ref(1)
@@ -13,10 +20,9 @@ function onBlockClick() {
   moneyTotal.value += perClick.value
 }
 
-setInterval(() => { 
+setInterval(() => {
   moneyTotal.value += perSec.value
 }, 1000)
-
 </script>
 
 <template>
